5e1cc4c8ae13533f74dc1e8527acea681850cc28
khermano <khermano@redhat.com>
2023-09-21 10:42:07 +0200

    Adding UserService, UserServiceImpl and security dependency

---
+++ user-service/src/main/java/cz/fi/muni/userservice/service/UserService.java
@@ -0,0 +1,37 @@
+package cz.fi.muni.userservice.service;
+
+import cz.fi.muni.userservice.persistence.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * An interface that defines a service access to the {@link User} entity.
+ */
+@Service
+public interface UserService {
+
+	/**
+	 * Register the given user with the given unencrypted password.
+	 */
+	void registerUser(User u, String unencryptedPassword);
+
+	/**
+	 * Get all registered users
+	 */
+	List<User> getAllUsers();
+
+	/**
+	 * Try to authenticate a user. Return true only if the hashed password matches the records.
+	 */
+	boolean authenticate(User u, String password);
+
+	/**
+	 * Check if the given user is admin.
+	 */
+	boolean isAdmin(User u);
+
+	User findUserById(Long userId);
+
+	User findUserByEmail(String email);
+}
---
+++ user-service/src/main/java/cz/fi/muni/userservice/service/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package cz.fi.muni.userservice.service;
+
+
+import cz.fi.muni.userservice.persistence.dao.UserDao;
+import cz.fi.muni.userservice.persistence.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Implementation of the {@link UserService}. This class is part of the service module of the application that provides the implementation of the
+ * business logic (main logic of the application).
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDao userDao;
+
+    // Argon2 is the best Key Derivation Function since 2015
+    private final PasswordEncoder encoder = new Argon2PasswordEncoder();
+
+    @Override
+    public void registerUser(User u, String unencryptedPassword) {
+        u.setPasswordHash(encoder.encode(unencryptedPassword));
+        userDao.create(u);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.findAll();
+    }
+
+    @Override
+    public boolean authenticate(User u, String password) {
+        return encoder.matches(password, u.getPasswordHash());
+    }
+
+    @Override
+    public boolean isAdmin(User u) {
+        //must get a fresh copy from database
+        return findUserById(u.getId()).isAdmin();
+    }
+
+    @Override
+    public User findUserById(Long userId) {
+        return userDao.findById(userId);
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return userDao.findUserByEmail(email);
+    }
+
+}
