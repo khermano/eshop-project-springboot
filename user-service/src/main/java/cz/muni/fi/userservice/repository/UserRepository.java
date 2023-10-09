e7f63eb21bf0253680a7cfdb51961d23252a48f0
khermano <khermano@redhat.com>
2023-10-09 18:18:20 +0200

    Renaming the UserDao to UserRepository and changing it to fit our project

---
+++ user-service/src/main/java/cz/muni/fi/userservice/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.userservice.repository;
+
+import cz.muni.fi.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	public User findByEmail(String email);
+}
