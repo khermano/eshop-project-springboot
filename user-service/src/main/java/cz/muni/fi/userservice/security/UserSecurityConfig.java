54bbd0fdab21f926ace83079ac9e0749d1212b9e
khermano <khermano@redhat.com>
2023-10-25 17:02:37 +0200

    Adding response 401 as in the original project

--- user-service/src/main/java/cz/muni/fi/userservice/security/UserSecurityConfig.java
+++ user-service/src/main/java/cz/muni/fi/userservice/security/UserSecurityConfig.java
@@ -1,17 +1,20 @@
 package cz.muni.fi.userservice.security;
 
 import cz.muni.fi.userservice.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import cz.muni.fi.userservice.entity.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -54,10 +57,19 @@ public class UserSecurityConfig {
         http.authorizeHttpRequests(requests -> requests
                 .requestMatchers("/user/*").hasRole("ADMIN")
                                 .anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults())
                 // returns 401 - inspired by https://stackoverflow.com/questions/72789433/change-spring-security-to-return-401-when-authentication-fails
-                .exceptionHandling(x -> x.accessDeniedHandler((request, response, exception) -> response.sendError(HttpStatus.UNAUTHORIZED.value(), exception.getMessage())));
+                .exceptionHandling(x -> x.accessDeniedHandler((request, response, exception) -> setUp401Response(response, exception)))
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    private void setUp401Response(HttpServletResponse response, AccessDeniedException exception) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setHeader("WWW-Authenticate", "Basic realm=\"type email and password\"");
+        response.getWriter().println("<html><body><h1>401 Unauthorized</h1> Go away ...</body></html>");
+        // all above seem to be uneffective
+        response.sendError(HttpStatus.UNAUTHORIZED.value(), exception.getMessage());
     }
 }
