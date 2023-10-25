aa66c9aebfb26522a9a24d2e4aa68b7c1566886a
khermano <khermano@redhat.com>
2023-10-25 16:37:49 +0200

    Returning 401

--- user-service/src/main/java/cz/muni/fi/userservice/security/UserSecurityConfig.java
+++ user-service/src/main/java/cz/muni/fi/userservice/security/UserSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import cz.muni.fi.userservice.entity.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -53,7 +54,9 @@ public class UserSecurityConfig {
         http.authorizeHttpRequests(requests -> requests
                 .requestMatchers("/user/*").hasRole("ADMIN")
                                 .anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+                // returns 401 - inspired by https://stackoverflow.com/questions/72789433/change-spring-security-to-return-401-when-authentication-fails
+                .exceptionHandling(x -> x.accessDeniedHandler((request, response, exception) -> response.sendError(HttpStatus.UNAUTHORIZED.value(), exception.getMessage())));
 
         return http.build();
     }
