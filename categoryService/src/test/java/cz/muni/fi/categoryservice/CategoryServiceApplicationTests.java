7bd7e301913cf2033dfd506a9ee724f4bec172d2
khermano <khermano@redhat.com>
2023-10-26 13:04:13 +0200

    Creating categoryService project

---
+++ categoryService/src/test/java/cz/muni/fi/categoryservice/CategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.categoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
