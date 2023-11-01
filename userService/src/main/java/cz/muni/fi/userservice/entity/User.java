3a11f8f86cd42acb7624018fe0fb7f5841d1e91e
khermano <khermano@redhat.com>
2023-11-01 23:01:47 +0100

    Correcting typo in User class

--- userService/src/main/java/cz/muni/fi/userservice/entity/User.java
+++ userService/src/main/java/cz/muni/fi/userservice/entity/User.java
@@ -13,7 +13,7 @@ import jakarta.validation.constraints.Pattern;
 import java.util.Date;
 
 @Entity
-//In Derby, its forbiden to 'USER' is reserved keyword, we need to rename table
+//In Derby, 'USER' is reserved keyword, we need to rename table
 @Table(name="Users")
 public class User {
 	@Id
