8976f724ef8a77d3690ebb247860e15c85884405
khermano <khermano@redhat.com>
2023-11-02 15:02:30 +0100

    [priceService] Replacing Tuple with AbstractMap.SimpleEntry

--- priceService/src/main/java/cz/muni/fi/priceService/service/impl/ExchangeServiceImpl.java
+++ priceService/src/main/java/cz/muni/fi/priceService/service/impl/ExchangeServiceImpl.java
@@ -2,37 +2,37 @@ package cz.muni.fi.priceService.service.impl;
 
 import cz.muni.fi.priceService.enums.Currency;
 import cz.muni.fi.priceService.service.ExchangeService;
-import cz.muni.fi.priceService.utils.Tuple;
 import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
+import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class ExchangeServiceImpl implements ExchangeService {
-	private static final Map<Tuple<Currency, Currency>, BigDecimal> currencyRateCache = new HashMap<>();
+	private static final Map<AbstractMap.SimpleEntry<Currency, Currency>, BigDecimal> currencyRateCache = new HashMap<>();
 
 	static {
 
-		Tuple<Currency, Currency> czk_usd = new Tuple<>(Currency.CZK,
+		AbstractMap.SimpleEntry<Currency, Currency> czk_usd = new AbstractMap.SimpleEntry<>(Currency.CZK,
 				Currency.USD);
-		Tuple<Currency, Currency> czk_eur = new Tuple<>(Currency.CZK,
+		AbstractMap.SimpleEntry<Currency, Currency> czk_eur = new AbstractMap.SimpleEntry<>(Currency.CZK,
 				Currency.EUR);
-		Tuple<Currency, Currency> czk_czk = new Tuple<>(Currency.CZK,
+		AbstractMap.SimpleEntry<Currency, Currency> czk_czk = new AbstractMap.SimpleEntry<>(Currency.CZK,
 				Currency.CZK);
 
-		Tuple<Currency, Currency> usd_czk = new Tuple<>(Currency.USD,
+		AbstractMap.SimpleEntry<Currency, Currency> usd_czk = new AbstractMap.SimpleEntry<>(Currency.USD,
 				Currency.CZK);
-		Tuple<Currency, Currency> usd_eur = new Tuple<>(Currency.USD,
+		AbstractMap.SimpleEntry<Currency, Currency> usd_eur = new AbstractMap.SimpleEntry<>(Currency.USD,
 				Currency.EUR);
-		Tuple<Currency, Currency> usd_usd = new Tuple<>(Currency.USD,
+		AbstractMap.SimpleEntry<Currency, Currency> usd_usd = new AbstractMap.SimpleEntry<>(Currency.USD,
 				Currency.USD);
 
-		Tuple<Currency, Currency> eur_usd = new Tuple<>(Currency.EUR,
+		AbstractMap.SimpleEntry<Currency, Currency> eur_usd = new AbstractMap.SimpleEntry<>(Currency.EUR,
 				Currency.USD);
-		Tuple<Currency, Currency> eur_czk = new Tuple<>(Currency.EUR,
+		AbstractMap.SimpleEntry<Currency, Currency> eur_czk = new AbstractMap.SimpleEntry<>(Currency.EUR,
 				Currency.CZK);
-		Tuple<Currency, Currency> eur_eur = new Tuple<>(Currency.EUR,
+		AbstractMap.SimpleEntry<Currency, Currency> eur_eur = new AbstractMap.SimpleEntry<>(Currency.EUR,
 				Currency.EUR);
 
 		currencyRateCache.put(czk_czk, BigDecimal.valueOf(1, 0));
@@ -50,7 +50,7 @@ public class ExchangeServiceImpl implements ExchangeService {
 
 	@Override
 	public BigDecimal getCurrencyRate(Currency currencyFrom, Currency currencyTo) {
-		Tuple<Currency, Currency> convertCouple = new Tuple<>(currencyFrom,
+		AbstractMap.SimpleEntry<Currency, Currency> convertCouple = new AbstractMap.SimpleEntry<>(currencyFrom,
 				currencyTo);
 		if (!currencyRateCache.containsKey(convertCouple)) {
 			throw new IllegalArgumentException(
