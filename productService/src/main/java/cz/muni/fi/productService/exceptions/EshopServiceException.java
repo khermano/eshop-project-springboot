b8ecb2ef728c7e7829e654a0a9b7ccebbf157955
khermano <khermano@redhat.com>
2023-11-06 16:19:37 +0100

    [productService] Commenting for now unused code inside the EshopServiceException

--- productService/src/main/java/cz/muni/fi/productService/exceptions/EshopServiceException.java
+++ productService/src/main/java/cz/muni/fi/productService/exceptions/EshopServiceException.java
@@ -2,25 +2,25 @@ package cz.muni.fi.productService.exceptions;
 
 public class EshopServiceException extends RuntimeException{
 
-	public EshopServiceException() {
-		super();
-	}
-
-	public EshopServiceException(String message, Throwable cause,
-                                 boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
-	public EshopServiceException(String message, Throwable cause) {
-		super(message, cause);
-	}
+//	public EshopServiceException() {
+//		super();
+//	}
+//
+//	public EshopServiceException(String message, Throwable cause,
+//                                 boolean enableSuppression, boolean writableStackTrace) {
+//		super(message, cause, enableSuppression, writableStackTrace);
+//	}
+//
+//	public EshopServiceException(String message, Throwable cause) {
+//		super(message, cause);
+//	}
 
 	public EshopServiceException(String message) {
 		super(message);
 	}
 
-	public EshopServiceException(Throwable cause) {
-		super(cause);
-	}
+//	public EshopServiceException(Throwable cause) {
+//		super(cause);
+//	}
 
 }
