9093a782a874bee8fcc392ff0530074f20651f61
khermano <khermano@redhat.com>
2023-11-06 17:32:21 +0100

    [productService] Editing ProductServiceTest -> 1 test failing

--- productService/src/test/java/cz/muni/fi/productService/service/ProductServiceTest.java
+++ productService/src/test/java/cz/muni/fi/productService/service/ProductServiceTest.java
@@ -1,102 +1,74 @@
 package cz.muni.fi.productService.service;
 
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import java.math.BigDecimal;
-import java.util.Calendar;
-import java.util.Date;
+import cz.muni.fi.productService.entity.Price;
+import cz.muni.fi.productService.entity.Product;
+import cz.muni.fi.productService.enums.Currency;
+import cz.muni.fi.productService.exceptions.EshopServiceException;
+import cz.muni.fi.productService.repository.PriceRepository;
+import cz.muni.fi.productService.service.impl.ProductServiceImpl;
 import org.hibernate.service.spi.ServiceException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-import cz.fi.muni.pa165.dao.PriceRepository;
-import cz.fi.muni.pa165.dao.ProductDao;
-import cz.fi.muni.pa165.entity.Price;
-import cz.fi.muni.pa165.entity.Product;
-import cz.fi.muni.pa165.enums.Currency;
-import cz.fi.muni.pa165.exceptions.EshopServiceException;
-import cz.fi.muni.pa165.service.config.ServiceConfiguration;
 
-@ContextConfiguration(classes=ServiceConfiguration.class)
-public class ProductServiceTest extends AbstractTransactionalTestNGSpringContextTests
-{
-    @Mock
-    private ProductDao productDao;
-    
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTest {
     @Mock
     private PriceRepository priceRepository;
-    @Mock
-    private TimeService timeService;
-     
-    @Mock
-    private ExchangeService exchangeService;
 
-    @Autowired
+//    @Autowired
     @InjectMocks
-    private ProductService productService;
+    private ProductServiceImpl productService;
 
-    @BeforeClass
+    @BeforeEach
     public void setup() throws ServiceException
     {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
     }
     
     private Product testProduct;
     
-    private Date fabricatedTime;
-    
-    @BeforeMethod
+    @BeforeEach
     public void prepareTestProduct(){
     	testProduct = new Product();
         Price price = new Price();
         price.setCurrency(Currency.EUR);
         price.setValue(new BigDecimal(10));
         testProduct.setCurrentPrice(price);
-        
-        when(exchangeService.getCurrencyRate(Currency.EUR, Currency.CZK)).thenReturn(BigDecimal.valueOf(27));
-    }
-    
-    @BeforeMethod
-    public void prepareFabricatedTime(){
-    	Calendar cal = Calendar.getInstance();
-    	cal.set(2015,2,2,0,0,0);
-    	fabricatedTime = cal.getTime();
-    	
-    	when(timeService.getCurrentTime()).thenReturn(fabricatedTime);
     }
     
     @Test
-    public void getPricevalueInCurrency(){
+    public void getPriceValueInCurrency(){
     	Product p = new Product();
         Price price = new Price();
         price.setCurrency(Currency.CZK);
         price.setValue(new BigDecimal(27));
         p.setCurrentPrice(price);
         
-        when(exchangeService.getCurrencyRate(Currency.CZK, Currency.CZK)).thenReturn(BigDecimal.ONE);
-        when(exchangeService.getCurrencyRate(Currency.CZK, Currency.EUR)).thenReturn(BigDecimal.valueOf(0.0370));
-        
         BigDecimal value = productService.getPriceValueInCurrency(p, Currency.CZK);
-        Assert.assertTrue(value.compareTo(BigDecimal.valueOf(27, 0)) == 0);
+        Assertions.assertTrue(value.compareTo(BigDecimal.valueOf(27, 0)) == 0);
         
-        value=productService.getPriceValueInCurrency(p, Currency.EUR);
-        Assert.assertTrue(value.compareTo(BigDecimal.valueOf(1, 0)) == 0,value.toPlainString());
+        value = productService.getPriceValueInCurrency(p, Currency.EUR);
+        Assertions.assertTrue(value.compareTo(BigDecimal.valueOf(1, 0)) == 0,value.toPlainString());
     }
-    
-    
-    @Test(expectedExceptions=EshopServiceException.class)
-    public void priceChangebyTooMuch(){
+
+    @Test
+    public void priceChangeByTooMuch(){
         Price newPrice = new Price();
         newPrice.setCurrency(Currency.CZK);
         newPrice.setValue(BigDecimal.valueOf(298));
-        productService.changePrice(testProduct, newPrice);
+        Assertions.assertThrows(EshopServiceException.class, () -> {
+            productService.changePrice(testProduct, newPrice);
+        });
     }
     
     @Test
@@ -107,8 +79,7 @@ public class ProductServiceTest extends AbstractTransactionalTestNGSpringContext
         productService.changePrice(testProduct, newPrice);
         
         verify(priceRepository).save(newPrice);
-        Assert.assertEquals(testProduct.getCurrentPrice(), newPrice);
-        Assert.assertEquals(testProduct.getCurrentPrice().getPriceStart(), fabricatedTime);
+        Assertions.assertEquals(testProduct.getCurrentPrice(), newPrice);
+//        Assertions.assertEquals(testProduct.getCurrentPrice().getPriceStart(), newPrice.getPriceStart());
     }
-
 }
