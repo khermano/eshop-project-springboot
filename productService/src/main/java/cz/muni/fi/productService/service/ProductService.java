f319979507a71a6bf9a2e80b86c554dc0926007e
khermano <khermano@redhat.com>
2023-11-06 15:25:59 +0100

    [productService] Editing ProductService

--- productService/src/main/java/cz/muni/fi/productService/service/ProductService.java
+++ productService/src/main/java/cz/muni/fi/productService/service/ProductService.java
@@ -2,24 +2,18 @@ package cz.muni.fi.productService.service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import cz.muni.fi.productService.entity.Price;
+import cz.muni.fi.productService.entity.Product;
+import cz.muni.fi.productService.enums.Currency;
 
-import org.springframework.stereotype.Service;
-
-import cz.fi.muni.pa165.entity.Category;
-import cz.fi.muni.pa165.entity.Price;
-import cz.fi.muni.pa165.entity.Product;
-import cz.fi.muni.pa165.enums.Currency;
-
-
-@Service
 public interface ProductService {
-	public Product findById(Long id);
-	public List<Product> findAll();
-	public Product createProduct(Product p);
-	public void addCategory(Product product, Category category);
-	public void removeCategory(Product product, Category category);
-	public void changePrice(Product product, Price newPrice);
-	public void deleteProduct(Product p);
-	public BigDecimal getPriceValueInCurrency(Product p, Currency currency);
+	Product findById(Long id);
+	List<Product> findAll();
+	Product createProduct(Product p);
+	void addCategory(Product product, Long categoryId);
+	void removeCategory(Product product, Long categoryId);
+	void changePrice(Product product, Price newPrice);
+	void deleteProduct(Product p);
+	BigDecimal getPriceValueInCurrency(Product p, Currency currency);
 	
 }
