237c47e880e8fe5173418792605b38da5eb951f4
khermano <khermano@redhat.com>
2023-11-10 14:33:43 +0100

    [categotyService] Copying CategoriesControllerTest from the original project and renaming it to CategoryControllerTest

---
+++ categoryService/src/test/java/cz/muni/fi/categoryservice/controller/CategoryControllerTest.java
@@ -0,0 +1,105 @@
+package cz.muni.fi.categoryservice.controller;
+
+import cz.fi.muni.pa165.RootWebContext;
+import cz.fi.muni.pa165.dto.CategoryDTO;
+import cz.fi.muni.pa165.facade.CategoryFacade;
+import cz.fi.muni.pa165.rest.controllers.CategoriesController;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebAppConfiguration
+@ContextConfiguration(classes = {RootWebContext.class})
+public class CategoryControllerTest extends AbstractTestNGSpringContextTests {
+    
+    @Mock
+    private CategoryFacade categoryFacade;
+
+    @Autowired
+    @InjectMocks
+    private CategoriesController categoriesController;
+
+    private MockMvc mockMvc;
+
+    @BeforeClass
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+       mockMvc = standaloneSetup(categoriesController).setMessageConverters(new MappingJackson2HttpMessageConverter()).build();
+    
+    }
+
+    @Test
+    public void getAllCategories() throws Exception {
+
+        doReturn(Collections.unmodifiableList(this.createCategories())).when(categoryFacade).getAllCategories();
+
+        mockMvc.perform(get("/categories"))
+                .andExpect(status().isOk())
+                .andExpect(
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.[?(@.id==1)].name").value("Electronics"))
+                .andExpect(jsonPath("$.[?(@.id==2)].name").value("Home Appliances"));
+
+    }
+
+    @Test
+    public void getValidCategory() throws Exception {
+
+        List<CategoryDTO> categories = this.createCategories();
+
+        doReturn(categories.get(0)).when(categoryFacade).getCategoryById(1l);
+        doReturn(categories.get(1)).when(categoryFacade).getCategoryById(2l);
+
+        mockMvc.perform(get("/categories/1"))
+                .andExpect(status().isOk())
+                .andExpect(
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.name").value("Electronics"));
+
+        mockMvc.perform(get("/categories/2"))
+                .andExpect(status().isOk())
+                .andExpect(
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.name").value("Home Appliances"));
+
+    }
+
+    @Test
+    public void getInvalidCategory() throws Exception {
+        doReturn(null).when(categoryFacade).getCategoryById(1l);
+
+        mockMvc.perform(get("/categories/1"))
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    private List<CategoryDTO> createCategories() {
+        CategoryDTO catOne = new CategoryDTO();
+        catOne.setId(1l);
+        catOne.setName("Electronics");
+
+        CategoryDTO catTwo = new CategoryDTO();
+        catTwo.setId(2l);
+        catTwo.setName("Home Appliances");
+
+        return Arrays.asList(catOne, catTwo);
+    }
+}
