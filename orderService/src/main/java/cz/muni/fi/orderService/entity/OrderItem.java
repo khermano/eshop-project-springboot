88576fc248be17303559981c878d442727a06c79
khermano <khermano@redhat.com>
2023-11-13 09:31:18 +0100

    [orderService] Copying Order, OrderItem and OrderState from the original project, adding spring-boot-starter-data-jpa and spring-boot-starter-validation dependency to pom.xml

---
+++ orderService/src/main/java/cz/muni/fi/orderService/entity/OrderItem.java
@@ -0,0 +1,92 @@
+package cz.muni.fi.orderService.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="ORDER_ITEM")
+public class OrderItem {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne
+	private Product product;
+		
+	private Integer amount;
+
+	@OneToOne
+	@JoinTable(name="ORDER_ITEM_PRICE")
+	private Price pricePerItem;
+	
+	
+	public Price getPricePerItem() {
+		return pricePerItem;
+	}
+
+	public void setPricePerItem(Price pricePerItem) {
+		this.pricePerItem = pricePerItem;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((amount == null) ? 0 : amount.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (amount == null) {
+			if (other.amount != null)
+				return false;
+		} else if (!amount.equals(other.amount))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		return true;
+	}
+
+	
+}
