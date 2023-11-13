295abda76996fe89557cdb96dc68b8dd6a0c38db
khermano <khermano@redhat.com>
2023-11-13 11:34:47 +0100

    [orderService] Editing OrderService, adding PriceDTO and Currency classes

---
+++ orderService/src/main/java/cz/muni/fi/orderService/dto/PriceDTO.java
@@ -0,0 +1,58 @@
+package cz.muni.fi.orderService.dto;
+
+import cz.muni.fi.orderService.enums.Currency;
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class PriceDTO {
+	private Long id;
+	private BigDecimal value;
+	private Currency currency;
+	private Date priceStart;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public BigDecimal getValue() {
+		return value;
+	}
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+	public Currency getCurrency() {
+		return currency;
+	}
+	public void setCurrency(Currency currency) {
+		this.currency = currency;
+	}
+    public Date getPriceStart() {
+        return priceStart;
+    }
+    public void setPriceStart(Date priceStart) {
+        this.priceStart = priceStart;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PriceDTO priceDTO = (PriceDTO) o;
+
+        if (value != null ? !value.equals(priceDTO.value) : priceDTO.value != null) return false;
+        if (currency != priceDTO.currency) return false;
+        return !(priceStart != null ? !priceStart.equals(priceDTO.priceStart) : priceDTO.priceStart != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = value != null ? value.hashCode() : 0;
+        result = 31 * result + (currency != null ? currency.hashCode() : 0);
+        result = 31 * result + (priceStart != null ? priceStart.hashCode() : 0);
+        return result;
+    }
+}
--- orderService/src/main/java/cz/muni/fi/orderService/service/OrderService.java
+++ orderService/src/main/java/cz/muni/fi/orderService/service/OrderService.java
@@ -1,44 +1,41 @@
 package cz.muni.fi.orderService.service;
 
-import cz.fi.muni.pa165.entity.Order;
-import cz.fi.muni.pa165.entity.Price;
-import cz.fi.muni.pa165.entity.User;
-import cz.fi.muni.pa165.enums.Currency;
-import cz.fi.muni.pa165.enums.OrderState;
-
+import cz.muni.fi.orderService.dto.PriceDTO;
+import cz.muni.fi.orderService.entity.Order;
+import cz.muni.fi.orderService.enums.Currency;
+import cz.muni.fi.orderService.enums.OrderState;
 import java.util.List;
 
 /**
- * An interface that defines a service access to the {@link Order} entity.
+ * An interface that defines service access to the {@link Order} entity.
  */
 public interface OrderService {
-
 	void createOrder(Order order);
 
 	/**
 	 * Get all saved orders belonging to the given user.
 	 */
-	List<Order> getOrdersByUser(User user);
+	List<Order> getOrdersByUserId(Long userId);
 	
 	/**
 	 * Get all orders saved within last week that have the given state.
 	 */
 	List<Order> getAllOrdersLastWeek(OrderState state);
 
-	
 	/**
 	 * Get all orders with the given state.
 	 */
 	List<Order> getOrdersByState(OrderState state);
 
-
 	List<Order> findAllOrders();
 
 	void shipOrder(Order order);
+
 	void finishOrder(Order order);
+
 	void cancelOrder(Order order);
 
 	Order findOrderById(Long id);
 
-	Price getTotalPrice(long orderId, Currency currency);
+	PriceDTO getTotalPrice(long orderId, Currency currency);
 }
