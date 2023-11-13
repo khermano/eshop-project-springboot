e2818e5586e55f96531e71600e2129474cc1350e
khermano <khermano@redhat.com>
2023-11-13 10:34:18 +0100

    [orderService] Editing OrderRepository - removing inherited methods, using Query and renaming methods to not have to implement them

--- orderService/src/main/java/cz/muni/fi/orderService/repository/OrderRepository.java
+++ orderService/src/main/java/cz/muni/fi/orderService/repository/OrderRepository.java
@@ -1,19 +1,17 @@
 package cz.muni.fi.orderService.repository;
 
-import cz.fi.muni.pa165.entity.Order;
-import cz.fi.muni.pa165.entity.User;
-import cz.fi.muni.pa165.enums.OrderState;
-
+import cz.muni.fi.orderService.entity.Order;
+import cz.muni.fi.orderService.enums.OrderState;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.Date;
 import java.util.List;
 
-public interface OrderRepository {
-	public void create(Order order);
-	public List<Order> findAll();
-	public List<Order> findByUser(User u);
-	public Order findById(Long id);
-	public void remove(Order o)  throws IllegalArgumentException;
-	public List<Order> getOrdersWithState(OrderState state);
-	public List<Order> getOrdersCreatedBetween(Date start, Date end, OrderState state);
-	
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	List<Order> findByUserId(Long userId);
+
+	List<Order> findByState(OrderState state);
+
+	@Query(value = "SELECT o FROM Order o WHERE o.state = :state AND  o.created BETWEEN :startDate AND :endDate")
+	List<Order> getOrdersCreatedBetween(Date start, Date end, OrderState state);
 }
