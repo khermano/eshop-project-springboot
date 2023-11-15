95e1d867e974e735f379f522c49bf46c3e4d2d58
khermano <khermano@redhat.com>
2023-11-15 16:04:23 +0100

    [categoryService] CategoryController -> adding ResponseEntity. changing RequestMapping to GetMapping and other

--- categoryService/src/main/java/cz/muni/fi/categoryservice/controller/CategoryController.java
+++ categoryService/src/main/java/cz/muni/fi/categoryservice/controller/CategoryController.java
@@ -7,11 +7,14 @@ import cz.muni.fi.categoryservice.repository.CategoryRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
@@ -35,11 +38,11 @@ public class CategoryController {
      *
      * @return list of Categories
      */
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public final List<Category> getCategories() {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public final ResponseEntity<List<Category>> getCategories() {
         logger.debug("rest getCategories()");
 
-        return categoryRepository.findAll();
+        return new ResponseEntity<>(categoryRepository.findAll(), HttpStatus.OK);
     }
 
     /**
@@ -51,13 +54,13 @@ public class CategoryController {
      * @return Category with given ID
      * @throws ResourceNotFoundException if category with given ID does not exist
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public final Category getCategory(@PathVariable("id") long id) throws ResourceNotFoundException {
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public final ResponseEntity<Category> getCategory(@PathVariable("id") long id) throws ResourceNotFoundException {
         logger.debug("rest getCategory({})", id);
 
         Optional<Category> category = categoryRepository.findById(id);
         if (category.isPresent()) {
-            return category.get();
+            return new ResponseEntity<>(category.get(), HttpStatus.OK);
         } else {
             throw new ResourceNotFoundException();
         }
@@ -76,24 +79,25 @@ public class CategoryController {
      * @return the created category
      * @throws ResourceAlreadyExistingException if for some reason we fail to create product with given info
      */
-    @RequestMapping(value = "/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public final Category createCategory(@RequestBody Category categoryInfo) throws ResourceAlreadyExistingException {
+    public final ResponseEntity<Category> createCategory(@RequestBody Category categoryInfo) throws ResourceAlreadyExistingException {
         logger.debug("rest createCategory()");
 
         Optional<Category> category = categoryRepository.findById(categoryInfo.getId());
         if (category.isPresent()) {
-            throw new ResourceAlreadyExistingException();
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
         if (categoryInfo.getId() == null || categoryInfo.getName() == null || categoryInfo.getName().isEmpty()) {
-            throw new IllegalArgumentException();
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
 
         try {
             categoryRepository.save(categoryInfo);
-            return categoryRepository.findById(categoryInfo.getId()).get();
+            category = categoryRepository.findById(categoryInfo.getId());
+            return new ResponseEntity<>(category.get(), HttpStatus.OK);
         } catch (Exception e) {
-            throw new ResourceAlreadyExistingException();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
